eea1008dffdd9ffa547c9186103370d3d7175b59
shreyans <shreyansms@gmail.com>
2024-09-08 13:33:14 -0700

    restructured components to better suit layout

--- src/App.js
+++ src/App.js
@@ -12,6 +12,11 @@ function App() {
       <h2>
         CS5610 - 20596
       </h2>
+      <div class="button-container">
+        <a href='https://github.com/AxonBlAzE/CS5610-LandingPage'><button class="button"> Landing Page Repo <FaGithub/></button></a>
+        <a href='https://main--kanbas-webapp-sm.netlify.app/'><button class="button"> Go to Kanbas <BsArrowUpRightSquare/></button></a>
+        <a href='https://github.com/AxonBlAzE/CS5610-Kanbas'><button class="button"> Kanbas Repo <FaGithub/></button></a>
+      </div>
       <table>
         <thead>
           <tr>
@@ -32,13 +37,33 @@ function App() {
             description = "Created a landing page for CS5610 that will contain the hyperlinks to all the following assignments."
             url = 'https://main--cs5610-landingpage-shreyansmulkutkar.netlify.app/' 
           />
+          <TableRow 
+            name = "A2"
+            description = "Coming Soon..."
+            url = '' 
+          />
+          <TableRow 
+            name = "A3"
+            description = "Coming Soon..."
+            url = '' 
+          />
+          <TableRow 
+            name = "A4"
+            description = "Coming Soon..."
+            url = '' 
+          />
+          <TableRow 
+            name = "A5"
+            description = "Coming Soon..."
+            url = '' 
+          />
+          <TableRow 
+            name = "A6"
+            description = "Coming Soon..."
+            url = '' 
+          />
         </tbody>
       </table>
-      <div class="button-container">
-        <a href='https://github.com/AxonBlAzE/CS5610-LandingPage'><button class="button"> Landing Page Repo <FaGithub/></button></a>
-        <a href='https://main--kanbas-webapp-sm.netlify.app/'><button class="button"> Go to Kanbas <BsArrowUpRightSquare/></button></a>
-        <a href='https://github.com/AxonBlAzE/CS5610-Kanbas'><button class="button"> Kanbas Repo <FaGithub/></button></a>
-      </div>
     </div>
   );
 }
